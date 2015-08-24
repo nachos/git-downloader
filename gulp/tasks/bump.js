6632a41dcfe3633ee6519ff6c0e37c7189a46eac
Elad Bezalel <elad.bezalel@gmail.com>
2015-08-24 19:50:02 +0300

    Added jscs-nachos and updated dependencies

--- gulp/tasks/bump.js
+++ gulp/tasks/bump.js
@@ -7,18 +7,18 @@ module.exports = function (gulp) {
   gulp.task('bump:patch', function () {
     return gulp.src(config.manifests)
       .pipe(bump({type: 'patch'}))
-      .pipe(gulp.destination('./'));
+      .pipe(gulp.dest('./'));
   });
 
   gulp.task('bump:minor', function () {
     return gulp.src(config.manifests)
       .pipe(bump({type: 'minor'}))
-      .pipe(gulp.destination('./'));
+      .pipe(gulp.dest('./'));
   });
 
   gulp.task('bump:major', function () {
     return gulp.src(config.manifests)
       .pipe(bump({type: 'major'}))
-      .pipe(gulp.destination('./'));
+      .pipe(gulp.dest('./'));
   });
 };
